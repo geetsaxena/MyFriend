dca2108aaa9e57614047b48a8c65ce19b1565f27
Ashish <ashish.saxena25@gmail.com>
2020-11-20 21:23:16 -0500

    Added frineds name

--- src/main/java/MyFriend.java
+++ src/main/java/MyFriend.java
@@ -1,5 +1,6 @@
 public class MyFriend {
     public static void main(String[] args) {
         System.out.println("I have many friends");
+        System.out.println("Payal and Asmita are two of them");
     }
 }
